0c1597dc1d06ef027e28d2add322b48bc08af23b
JuanSevillano <sevi0_6@hotmail.com>
2021-11-27 17:18:42 +0100

    adding login structure and starting theming logic

---
+++ src/components/FieldCustom/FieldCustom.tsx
@@ -0,0 +1,24 @@
+import { TextField } from '@material-ui/core';
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledTextField = styled((props) => (
+	<TextField
+		{...props}
+		fullWidth
+		variant="outlined"
+	/>
+))`
+${({ theme }) => `
+	border-color: ${theme.palette.primary.main};
+	margin: 10px 0px; 
+`}`;
+
+
+const FieldCustom = (props: any) => {
+	return (
+		<StyledTextField props={props} />
+	)
+}
+
+export default FieldCustom
