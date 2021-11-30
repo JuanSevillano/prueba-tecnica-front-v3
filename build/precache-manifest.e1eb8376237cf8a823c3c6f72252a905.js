fe367ce65d4f85c68415674b2fac2fa5f594bae8
JuanSevillano <sevi0_6@hotmail.com>
2021-11-30 23:03:27 +0100

    :rocket: deploying to github pages

---
+++ build/precache-manifest.e1eb8376237cf8a823c3c6f72252a905.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74959cf20041f05c0948f8930226c600",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51ba47478bde4fd15125",
+    "url": "/static/css/main.bbdd240e.chunk.css"
+  },
+  {
+    "revision": "881a086c2270b6ea2d9e",
+    "url": "/static/js/2.bbcbfc0e.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/static/js/2.bbcbfc0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98f0a7f3dcd7bc5c8088",
+    "url": "/static/js/3.1953da1c.chunk.js"
+  },
+  {
+    "revision": "fd7525d544dd9c67d07855cb8778e590",
+    "url": "/static/js/3.1953da1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcfbf0cc39bda880d29a",
+    "url": "/static/js/4.2e6b3324.chunk.js"
+  },
+  {
+    "revision": "51ba47478bde4fd15125",
+    "url": "/static/js/main.04ed7eed.chunk.js"
+  },
+  {
+    "revision": "c45a1a612177713e6160",
+    "url": "/static/js/runtime-main.c7d4ae6c.js"
+  },
+  {
+    "revision": "29bd5dd41e18e6b4b51cafdcd8345dcc",
+    "url": "/static/media/color_logo.29bd5dd4.svg"
+  },
+  {
+    "revision": "b5439303fdc27af42b6186157d3785ed",
+    "url": "/static/media/white_logo.b5439303.svg"
+  }
+]);
